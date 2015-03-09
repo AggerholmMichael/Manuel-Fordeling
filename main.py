45f5ed53742b2e3742515cf6e4931fe6b4a5a6cf
MadsStilling <madsstilling@gmail.com>
2015-03-09 22:18:53 +0100

    Better naming

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Alle tal er i meter
 
 antal_paller = 0
-antal_ordrer = 0
+antal_produktion = 0
 
 class Palle():
 
     def __init__(self):
 		palle_id = ""						#String til identifikation af individuelle objecter
-		mængde = 0							#Beholder til volumen af råvare, bruges til udregning af fordelingsfaktor	
+		maengde = 0							#Beholder til volumen af råvare, bruges til udregning af fordelingsfaktor	
 		fordelingsfaktor = 0				#procentdelen som denne palle indeholder ud af indholdet fra ALLE paller i ordren.
 
 
