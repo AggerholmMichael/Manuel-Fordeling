43297ba5c817dda8b0dbeb9d4f95f6f89924958c
StaticRebel <jegfatterhat@hotmail.com>
2015-03-09 22:11:31 +0100

    Palle class updated, id, mængde, fordelingsfaktor

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# Alle tal er i meter
 
 antal_paller = 0
 antal_ordrer = 0
 
 class Palle():
 
-    def __init__(self, antal_paller, ):
-
+    def __init__(self):
+		palle_id = ""						#String til identifikation af individuelle objecter
+		mængde = 0							#Beholder til volumen af råvare, bruges til udregning af fordelingsfaktor	
+		fordelingsfaktor = 0				#procentdelen som denne palle indeholder ud af indholdet fra ALLE paller i ordren.
 
 
 class Produktion():
