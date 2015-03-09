858330b028ca58e0f05d9401460078ca3a4c969e
MadsStilling <madsstilling@gmail.com>
2015-03-09 21:54:04 +0100

    minor commit

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ antal_paller = 0
 antal_ordrer = 0
 
 class Palle():
-	
-	def __init__(self, antal_paller, ):
-		
-		
+
+    def __init__(self, antal_paller, ):
+
+
 
 class Produktion():
-	pass
+    pass
