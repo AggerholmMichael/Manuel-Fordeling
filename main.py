7905306d5fc1c237680f72105fcde6fbf6edebf2
AggerholmMichael <jegfatterhat@hotmail.com>
2015-03-11 22:11:16 +0100

    Added instances of Palle to a dict, added user naming of paller, added user input on maengde, renamed the variable KEY to palle_id, removed unnecesary code

--- main.py
+++ main.py
@@ -3,42 +3,32 @@
 # Vi definerer at "Debug Besked" forkortes DB samt bruges.
 # 
 
-antal_paller = 0
-antal_produktioner = 0
-palle = {}
+antal_paller = 1			#Variabel til at gemme antal generede paller
+antal_produktioner = 0		#Variabel til at gemme antal generede produktioner
+palledict = {}				#Dictionary til at gemme vores paller i (Dette gælder åbentbart også objects!)
 class Palle():
 	
 	def __init__(self):
-		print "PALLE"
+		print "PALLE"		#DB
 		global antal_paller			#Giver adgang til variablen "antal_paller" i dette scope/class/def
 		antal_paller += 1		#Plus 1 stk palle pAA palle_antal
 		self.maengde = 0                    #Beholder til volumen af raavare, bruges til udregning af fordelingsfaktor	
 		self.omregningsfaktor = 0           #procentdelen som denne palle indeholder ud af indholdet fra ALLE paller i ordren.
 		
 class Produktion():
-	print "PRODUKTION"
-	produktions_id = ""
+	print "PRODUKTION"			#DB
+	produktions_id = ""			#Container til produktions-ID
 	kalkuleret_antal = 0					#Det forhaands kalkulerede maengde af raavarer som skal forbruges i denne produktion
 
-while antal_paller < 4:
-	key = antal_paller		#
-	palle[key] = Palle()
-	print palle[key]
-	palle[key].maengde += 5
-	print palle[key].maengde
-	#palle_abe = Palle()		#jeg laver en ny palle, entity navnet er en placeholder
-	print("if")          #DB
-	print(antal_paller)	#DB
-	#print(palle+antal_paller.int_palle_id) #DB
-	#print(palle+antal_paller.maengde) #DB
-	raw_input()					#Holder shell aaben :P venter paa tekst input
-		
-else:
-	print "else"	#DB
-	raw_input()					#Holder min CMD AAben :P		
-	print antal_paller	#DB
-	#print antal_produktioner	#DB
-	#print produktions_id		#DB
-	#print kalkuleret_antal		#DB
+while antal_paller < 2:
+
+	palle_id = input("Indtast Palle-ID: ")		#variabel oprettes, input fra bruger (Palle-ID'et) gemmes.
+	palledict[palle_id] = Palle()				#En Palle bliver spawned i vores dictionary, Palle-ID'et er dens navn :D
+	print str(palledict[palle_id]) + " Oprettet"	#DB
+	
+	palledict[palle_id].maengde = input("Indtast maengde: ")		#Pallens maengde tilgåes via dictionary, og ændres afhængig af bruger inputtet
+	print str(palledict[palle_id].maengde) + " Tilfoejet til pallen"	#DB
+	
+	#testpalle = Palle()		#DB
 
 raw_input()					#Holder min CMD AAben :P
